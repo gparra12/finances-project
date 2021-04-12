17e8a2c1ccd352b3df2a4974301b40a8194d203b
Guilherme Parra <parra.guilherme2@gmail.com>
2021-04-11 22:16:51 -0300

    :pencil: fixing bug in formatAmount()

--- script.js
+++ script.js
@@ -131,9 +131,9 @@ const Utils = { // utilidades
     },
 
     formatAmount(value) {
-        value = Number(value) * 100
+        value = value * 100
 
-        return value;
+        return Math.round(value)
     },
 
     formatDate(date) {
